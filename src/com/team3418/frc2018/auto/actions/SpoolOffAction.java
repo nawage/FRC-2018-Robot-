541312f97c28c38ba02690845f1e82343e912832
RoboRiotDev4 <RoboRiotDev4@gmail.com>
2018-03-13 20:01:38 -0500

    Auto Actions Shooter pt 2
    Ethan K -

--- src/com/team3418/frc2018/auto/actions/SpoolOffAction.java
+++ src/com/team3418/frc2018/auto/actions/SpoolOffAction.java
@@ -3,15 +3,13 @@ package com.team3418.frc2018.auto.actions;
 import com.team3418.frc2018.subsystems.MrCush;
 import com.team3418.frc2018.subsystems.Shooter;
 
-public class SpoolShootAction implements Action{
+public class SpoolOffAction implements Action{
 
-	private MrCush mMrCush;
 	private Shooter mShooter;
 	
 	private boolean finished = false;
 	
-	public SpoolShootAction() {
-		mMrCush = MrCush.getInstance();
+	public SpoolOffAction() {
 		mShooter = Shooter.getInstance();
 	}
 	
@@ -22,15 +20,13 @@ public class SpoolShootAction implements Action{
 
 	@Override
 	public void update() {
-		if mShooter.isShooterReady() {
-			mMrCush.Extend();
-		}
+		
 	}
 
 	@Override
 	public boolean isFinished() {
 		if (finished) {
-			System.out.println("Spool/Shoot Action Completed");
+			System.out.println("Spool Off Action Completed");
 			return true;
 		}
 		return false;
